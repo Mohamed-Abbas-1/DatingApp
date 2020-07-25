e775a02186f8b88ab63aaee016a40a049391beeb
Mohamed Abbas <Mohamed.abbas.cic@gmail.com>
2020-07-25 20:36:21 +0200

    End Of Section 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { emit } from 'process';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +15,9 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService, private  alertify: AlertifyService) { }
+  regError: string;
+  passError: string;
+  constructor(private authService: AuthService, private  alertify: AlertifyService,private router: Router) { }
 
   ngOnInit() {
   }
@@ -25,11 +28,28 @@ export class RegisterComponent implements OnInit {
       this.cancel();
     }, error => {
       this.alertify.error(error);
-    });
+      // this.regError = error;
+      this.errorHandling();
+    }, () => this.router.navigate(['/members']));
   }
+
 
   cancel(){
     this.cancelRegister.emit();
+  }
+
+  errorHandling(){
+    console.log('test');
+    if (this.model.password.length < 6) {
+      this.passError = 'Password should be at least 6';
+    }else if (this.model.password.length > 20) {
+      this.passError = 'Password should be less than 20';
+    }
+    if (this.model.username.length < 3) {
+      this.regError = 'User Name should be at least 6';
+    }else if (this.model.username.length > 12) {
+      this.regError = 'User Name should be less than 20';
+    }
   }
 
 }
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListsComponent } from './member-lists/member-lists.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '' ,
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+        { path: 'members', component: MemberListsComponent },
+        { path: 'lists', component: ListsComponent },
+        { path: 'messages', component: MessagesComponent },
+    ]},
+    { path: '**', redirectTo: '' , pathMatch: 'full' }
+]
+
+
+// { path: 'members', component: MemberListsComponent , canActivate: [AuthGuard] } // For single Auth.
